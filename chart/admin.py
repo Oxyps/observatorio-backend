04e703a9e272a541e75b4e959ade34655ea5e1a9
Oxyps <abel_castro_junior@hotmail.com>
2020-06-28 21:34:39 -0300

    added chart app admin pages for crud models

--- chart/admin.py
+++ chart/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import DataType, Granularity, ReferencePeriod, Location, Information, Data
+
+admin.site.register(DataType)
+admin.site.register(Granularity)
+admin.site.register(ReferencePeriod)
+admin.site.register(Location)
+admin.site.register(Information)
+admin.site.register(Data)
