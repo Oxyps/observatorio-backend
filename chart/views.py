bc2a1569ffd5b83c266ad66f0a2e278e17821e0f
Oxyps <abel_castro_junior@hotmail.com>
2020-06-28 21:29:31 -0300

    added chart app models listing views

--- chart/views.py
+++ chart/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Data, Information, Granularity, DataType, Location, ReferencePeriod
+from .serializers import DataSerializer, InformationSerializer, GranularitySerializer, DataTypeSerializer, LocationSerializer, ReferencePeriodSerializer
+
+class DataView(generics.ListAPIView):
+	queryset = Data.objects.all()
+	serializer_class = DataSerializer
+
+class InformationView(generics.ListAPIView):
+	queryset = Information.objects.all()
+	serializer_class = InformationSerializer
+
+class DataTypeView(generics.ListAPIView):
+	queryset = DataType.objects.all()
+	serializer_class = DataTypeSerializer
+
+class LocationView(generics.ListAPIView):
+	queryset = Location.objects.all()
+	serializer_class = LocationSerializer
+
+class GranularityView(generics.ListAPIView):
+	queryset = Granularity.objects.all()
+	serializer_class = GranularitySerializer
+
+class ReferencePeriodView(generics.ListAPIView):
+	queryset = ReferencePeriod.objects.all()
+	serializer_class = ReferencePeriodSerializer
