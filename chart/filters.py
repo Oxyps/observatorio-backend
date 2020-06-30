76a9f724f5670d86db7b2876316ffc7c95867de3
Oxyps <abel_castro_junior@hotmail.com>
2020-06-29 23:58:07 -0300

    fixed url filter labels

--- chart/filters.py
+++ chart/filters.py
@@ -5,7 +5,10 @@ from .models import ReferencePeriod, Data
 class ReferencePeriodFilter(filters.FilterSet):
 	class Meta:
 		model = ReferencePeriod
-		fields = ['in_date_gt', 'until_date_lte']
+		fields = [
+			'in_date_gt',
+			'until_date_lte'
+		]
 
 	in_date_gt = filters.DateFilter(field_name='in_date', lookup_expr='gt')
 	until_date_lte = filters.DateFilter(field_name='until_date', lookup_expr='lte')
@@ -14,10 +17,15 @@ class DataFilter(filters.FilterSet):
 	class Meta:
 		model = Data
 		fields = [
-			'id_information__nickname',
-			'id_location__name',
-			'id_granularity__granularity',
+			'information_nickname',
+			'location_name',
+			'granularity',
+			'in_date_gt',
+			'until_date_lte'
 		]
 
-	in_date_gt = filters.DateFilter(field_name='id_referenceperiod__in_date', lookup_expr='gt')
-	until_date_lte = filters.DateFilter(field_name='id_referenceperiod__until_date', lookup_expr='lte')
+	information_nickname = filters.CharFilter(field_name='id_information__nickname', label='information_nickname')
+	location_name = filters.CharFilter(field_name='id_location__name', label='location_name')
+	granularity = filters.CharFilter(field_name='id_granularity__granularity', label='granularity')
+	in_date_gt = filters.DateFilter(field_name='id_referenceperiod__in_date', label='in_date_gt', lookup_expr='gt')
+	until_date_lte = filters.DateFilter(field_name='id_referenceperiod__until_date', label='until_date_lte', lookup_expr='lte')
