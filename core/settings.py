b3b4e7a9eae36bedfe1cf439eb6c7a717d216dab
Oxyps <abel_castro_junior@hotmail.com>
2020-07-01 04:00:03 -0300

    added cors config

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
 	'rest_framework',
 	'django_filters',
+	'corsheaders',
 
 	'chart',
 ]
@@ -48,14 +49,23 @@ REST_FRAMEWORK = {
 	'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
 }
 
+CORS_ORIGIN_WHITELIST = [
+	'http://localhost:3000',
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+
+	# CORS middleware
+	'corsheaders.middleware.CorsMiddleware',
+
+	'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'core.urls'
