eb6ffce1b952a8ed3ef7afc52ad7d628ccb9e325
Oxyps <abel_castro_junior@hotmail.com>
2020-07-02 18:40:54 -0300

    fixed some indentation bug

--- chart/serializers.py
+++ chart/serializers.py
@@ -3,36 +3,26 @@ from rest_framework import serializers
 from .models import Data, Information, Granularity, DataType, Location, ReferencePeriod
 
 class DataSerializer(serializers.ModelSerializer):
-
     class Meta:
-
         model = Data
         fields = '__all__'
 
 class InformationSerializer(serializers.ModelSerializer):
-
     class Meta:
-
         model = Information
         fields = '__all__'
 
 class GranularitySerializer(serializers.ModelSerializer):
-
     class Meta:
-
         model = Granularity
         fields = '__all__'
 
 class LocationSerializer(serializers.ModelSerializer):
-
     class Meta:
-
         model = Location
         fields = '__all__'
 
 class ReferencePeriodSerializer(serializers.ModelSerializer):
-
     class Meta:
-
         model = ReferencePeriod
         fields = '__all__'
